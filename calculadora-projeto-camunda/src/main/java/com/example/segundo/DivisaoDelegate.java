b550cc0d7cdc4fdb0ba877abb18ec62dfd867ad8
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-03-12 11:50:20 -0300

    Outras operações

---
+++ calculadora-projeto-camunda/src/main/java/com/example/segundo/DivisaoDelegate.java
@@ -0,0 +1,23 @@
+package com.example.segundo;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class DivisaoDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        /*  var a = execution.getVariable("a")
+            var b = execution.getVariable("b")
+
+            execution.setVariable("resultado", a + b)
+
+            Praticamente os mesmos scripts feito no camunda, porém o
+            execution.getVariable() retorna um tipo Object.
+        */
+
+        var a = (Long)execution.getVariable("a");
+        var b = (Long)execution.getVariable("b");
+
+        execution.setVariable("resultado", a / b);
+    }
+}
